34642b2c1f805ea7c7582ed052015f8ae19dd307
Alvindo Tri Jatmiko <alvindo56@gmail.com>
2024-12-06 20:24:29 +0700

    fix: vercel configuration

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,31 @@
+import { json } from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import app from '../src/app';
+import express from 'express';
 import { initAdmin } from '../src/utils/init-admin';
+import { errorHandler } from '../src/middlewares/error';
+import cashierRoutes from '../src/routes/cashier-routes';
+import adminRoutes from '../src/routes/admin-routes';
+import authRoutes from '../src/routes/auth-routes';
+import productRoutes from '../src/routes/product-routes';
+import transactionRoutes from '../src/routes/transaction-routes';
 
 dotenv.config();
+const app = express();
 
 const port: number = parseInt(process.env.PORT!);
+
+// middleware
+app.use(json());
+app.use(cors());
+
+// routes
+app.use(errorHandler);
+app.use('/cashiers', cashierRoutes);
+app.use('/cashiers', adminRoutes);
+app.use('/auth', authRoutes);
+app.use('/products', productRoutes);
+app.use('/transactions', transactionRoutes);
 
 const startServer = async () => {
   try {
@@ -20,3 +41,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+module.exports = app;
